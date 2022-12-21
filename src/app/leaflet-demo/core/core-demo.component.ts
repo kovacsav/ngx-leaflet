932031dcc21ceec8c98a56d59ba654568bc4a7b3
kovacsav <mezbogyo@gmail.com>
2022-12-21 22:28:51 +0100

    center

--- src/app/leaflet-demo/core/core-demo.component.ts
+++ src/app/leaflet-demo/core/core-demo.component.ts
@@ -10,22 +10,22 @@ export class LeafletCoreDemoComponent {
 
 	optionsSpec: any = {
 		layers: [{ url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', attribution: 'Open Street Map' }],
-		zoom: 5,
-		center: [ 46.879966, -121.726909 ]
+		zoom: 6,
+		center: [47.11499982620772, 19.29128309899825]
 	};
 
 	// Leaflet bindings
 	zoom = this.optionsSpec.zoom;
 	center = latLng(this.optionsSpec.center);
 	options = {
-		layers: [ tileLayer(this.optionsSpec.layers[0].url, { attribution: this.optionsSpec.layers[0].attribution }) ],
+		layers: [tileLayer(this.optionsSpec.layers[0].url, { attribution: this.optionsSpec.layers[0].attribution })],
 		zoom: this.optionsSpec.zoom,
 		center: latLng(this.optionsSpec.center)
 	};
 
 	// Form bindings
 	formZoom = this.zoom;
-	zoomLevels = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ];
+	zoomLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 	lat = this.center.lat;
 	lng = this.center.lng;
 
